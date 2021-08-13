59399d18bbf654d258d149f555beb2adf447cbe1
Cameron Burrows <cameron.burrows@planetpayment.com>
2021-08-13 15:44:36 -0400

    added new librespot file

---
+++ src/algorythm/librespot_events.py
@@ -0,0 +1,12 @@
+import zmq
+import os
+
+if 'PLAYER_EVENT' in os.environ and os.environ['PLAYER_EVENT'] == 'playing':
+    msg = str(os.environ['TRACK_ID'])
+    context = zmq.Context()
+
+    socket = context.socket(zmq.REQ)
+    socket.connect("tcp://localhost:5555")
+
+    socket.send(bytes(msg,'utf-8'))
+    message = socket.recv()
